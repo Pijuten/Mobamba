2a02dbdc38a383c053557c7487a8607e051bf1a4
stani <stani1423@gmail.com>
2023-08-01 22:29:30 +0200

    "Initial commit"

---
+++ Source/Mobamba/Mobamba.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Mobamba.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Mobamba, "Mobamba" );
